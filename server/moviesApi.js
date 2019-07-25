bfa64a2391b2e6d67d2313a4a04db2c34cc4eb41
Kaisin Li <kaisinli@Kaisins-MacBook-Pro.local>
2019-07-25 15:00:43 -0400

    data submits

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
 const request = require('request');
 const { db } = require('../db');
+const multer = require ('multer');
 const OMDB_API_KEY = require('../apiKey')
 const OMDB_API = "http://www.omdbapi.com"
 
-router.post('/', (req, res) => {
-    request({
-        uri: OMDB_API,
-        qs: {
-            apikey: OMDB_API_KEY,
-            t: req.body.name
+const upload = multer();
+
+router.post('/', upload.fields([]), (req, res, next) => {
+    let title = req.body.name
+    console.log("===== ", req.body);
+    console.log('TITLE:', title);
+    request.post(`${OMDB_API}/?apikey=${OMDB_API_KEY}&t=${title}`, (err, response) => {
+        if (err) {
+            return next(err);
         }
-    }).pipe(res)
+        const movieInfo = response.body;
+        console.log(JSON.parse(movieInfo))
+        res.render('searchResult', { result: JSON.parse(movieInfo) });
+        // res.send({movieInfo})
+    });
 })
 
 module.exports = router
