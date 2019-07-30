a466a88dab7b977f3855e338eaeb567ee91e6fb4
Kaisin Li <kaisinli@Kaisins-MacBook-Pro.local>
2019-07-29 22:49:54 -0400

    final

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const request = require('request');
-<<<<<<< HEAD
 const db = require('../db');
 const OMDB_API_KEY = require('../apiKey')
 const OMDB_API = "http://www.omdbapi.com"
@@ -60,8 +59,6 @@ router.get('/favorites', (req, res, next) => {
                 return next(err);
             }
 
-            console.log(results)
-
             res.render('savedMovies', {results});
         });
     });
@@ -69,33 +66,3 @@ router.get('/favorites', (req, res, next) => {
 
 
 module.exports = router;
-=======
-const { db } = require('../db');
-const multer = require('multer');
-const OMDB_API_KEY = require('../apiKey')
-const OMDB_API = "http://www.omdbapi.com"
-
-const upload = multer();
-
-router.post('/', upload.fields([]), (req, res, next) => {
-    let title = req.body.name
-    request.post(`${OMDB_API}/?apikey=${OMDB_API_KEY}&t=${title}&plot=full`, (err, response) => {
-        if (err) {
-            return next(err);
-        }
-        const movieInfo = JSON.parse(response.body);
-        console.log(movieInfo)
-
-        if (movieInfo.Response === 'False') {
-            res.render('noResult')
-        } else {
-            if(movieInfo.Poster === 'N/A'){
-                movieInfo.Poster = 'http://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png'
-            }
-            res.render('searchResult', { result: movieInfo });
-        }
-    });
-})
-
-module.exports = router
->>>>>>> 862484d... api works
